d67f3be4149d1b9cfaa4f6828cb5566428fb6089
=nguyenson98 <nguyensonkhv26598@gmail.com>
2022-09-16 17:07:52 +0700

    Done home page

---
+++ src/assets/fake-data/policy.js
@@ -0,0 +1,24 @@
+const policy = [
+    {
+        name: "Miễn phí giao hàng",
+        description: "Miến phí ship với đơn hàng > 239k",
+        icon: "bx bx-shopping-bag"
+    },
+    {
+        name: "Thanh toán COD",
+        description: "Thánh toán khi nhận hàng (COD)",
+        icon: "bx bx-credit-card"
+    },
+    {
+        name: "Khách hàng VIP",
+        description: "Ưu đãi dành cho khách hàng VIP",
+        icon: "bx bx-diamond"
+    },
+    {
+        name: "Hỗ trợ bảo hành",
+        description: "Đổi, sửa đồ tại tất cả store",
+        icon: "bx bx-donate-heart"
+    },
+]
+
+export default policy;
